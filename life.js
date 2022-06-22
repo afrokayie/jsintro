21ba47daf8527a74754092efefc1e9de7c8365da
afrokayie <60403521+afrokayie@users.noreply.github.com>
2022-06-22 16:23:44 -0700

    Add files via upload

---
+++ life.js
@@ -0,0 +1,7 @@
+
+
+        document.write(" Name: Kayode Oluwole");
+        document.write("<br>");
+        document.write(" Height: 175 cm")
+        document.write("<br>");
+        document.write(" country: Nigeria");
